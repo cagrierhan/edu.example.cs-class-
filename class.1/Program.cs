02130eaa6747753e208a88a52c05d68219a0ee03
cagrierhan <53853189+cagrierhan@users.noreply.github.com>
2020-11-23 16:40:00 +0300

    Dersteki uygulama.

---
+++ class.1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Class
+{
+    class evHalkı
+    {
+        public string Name;
+        public int DogYil;
+        public string EyeColor;
+        public string Job;
+        public void Bilgi()
+        {
+            Console.WriteLine("İsim-Soyisim : {0}", Name);
+            Console.WriteLine("Doğum Yılı: {0}", DogYil);
+            Console.WriteLine("Yaşı : {0}", Age());
+            Console.WriteLine("İşiniz : {0}", Job);
+            Console.WriteLine("Göz Renginiz : {0}", EyeColor);
+            Console.WriteLine("===================================");
+        }
+        public int Age()
+        {
+            return 2020 - DogYil;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            evHalkı annem = new evHalkı();
+            {
+                annem.Name = "Arzu Erinmez";
+                annem.DogYil = 1980;
+                annem.Job = "Ticaret";
+                annem.EyeColor = "Kahverengi";
+                annem.Age();
+
+                annem.Bilgi();
+            }
+            evHalkı kardesim = new evHalkı();
+            {
+                kardesim.Name = "Çağla Yade Erhan";
+                kardesim.DogYil = 2000;
+                kardesim.Job = "Öğrenci";
+                kardesim.EyeColor = "Kahverengi";
+                kardesim.Age();
+                kardesim.Bilgi();
+            }
+            evHalkı ben = new evHalkı();
+            {
+                ben.Name = "Çağrı Erhan";
+                ben.DogYil = 1999;
+                ben.Job = "Öğrenci";
+                ben.EyeColor = "Kahverengi";
+                ben.Age();
+
+                ben.Bilgi();
+            }
+
+        }
+
+    }
+}
+
+
+
